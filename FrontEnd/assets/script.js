245041583679699bc88f2d7f2c4a720e09dfe65b
Laban G <guildeur@hotmail.fr>
2023-10-11 14:09:42 +0200

    rendu le code plus fonctionnel
    correction du renvois d'erreur du fetch, convertion de la fonction de generation des fiche d'une boucle for à un 'forEach'.
    Simplification diverse pour éviter les répétitions

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,150 +1,111 @@
 //  ----------  récupération projets
 
 async function fetchProjets() {
-    const r = await fetch("http://localhost:5678/api/works", {
+    const response = await fetch("http://localhost:5678/api/works", {
         method: 'GET',
         headers: {
             "Accept": "application/json"
         }
     })
-    if (r.ok === true) {
-        return r.json();
+    if (response.ok === true) {
+        return await response.json();
     }
-    else {
-        try {
-            throw new Error('Impossible de contacter le serveur')
-        }
-        catch (error) {
-            console.error(error);
-        }
-    }
+    throw new Error('Impossible de contacter le serveur')
 }
 
 // ----------  génération des fiches (functions)
 // afficher tous les projets
 const gallery = document.getElementById("gallery");
-function genererProjets(param) {
-    for (let i = 0; i < param.length; i++) {
-        const projetsElement = document.createElement("figure");
-        const imageElement = document.createElement("img");
-        const captionElement = document.createElement("figcaption");
-        imageElement.src = param[i].imageUrl;
-        captionElement.innerHTML = param[i].title;
-        projetsElement.appendChild(imageElement);
-        projetsElement.appendChild(captionElement);
-        gallery.appendChild(projetsElement);
-    }
+
+function displayProjets(projets) {
+    projets.forEach(projets => {
+        gallery.innerHTML += `
+        <figure>
+        <img src="${projets.imageUrl}" alt="${projets.title}">
+        <figcaption>${projets.title}</figcaption>
+    </figure>
+        `
+    });
 }
 
 // afficher les projets objets
-function genererObjets(param2) {
+function displayObjets(projets) {
     let projetObjets = [];
-    for (let i = 0; i < param2.length; i++) {
-        if (param2[i].categoryId == 1) {
-            projetObjets.push(param2[i]);
+    // projets.filter ( projets => projets.categoryId === 1)
+    for (let i = 0; i < projets.length; i++) {
+        if (projets[i].categoryId == 1) {
+            projetObjets.push(projets[i]);
         }
     }
-    for (let i = 0; i < projetObjets.length; i++) {
-        const projetsElement = document.createElement("figure");
-        const imageElement = document.createElement("img");
-        const captionElement = document.createElement("figcaption");
-        imageElement.src = projetObjets[i].imageUrl;
-        captionElement.innerHTML = projetObjets[i].title;
-        projetsElement.appendChild(imageElement);
-        projetsElement.appendChild(captionElement);
-        gallery.appendChild(projetsElement);
-    }
+    displayProjets(projetObjets);
 }
 
 // afficher les projets Appartements
-function genererAppart(param3) {
+function displayAppart(projets) {
     let projetAppart = [];
-    for (let i = 0; i < param3.length; i++) {
-        if (param3[i].categoryId == 2) {
-            projetAppart.push(param3[i]);
+    for (let i = 0; i < projets.length; i++) {
+        if (projets[i].categoryId == 2) {
+            projetAppart.push(projets[i]);
         }
     }
-    for (let i = 0; i < projetAppart.length; i++) {
-        const projetsElement = document.createElement("figure");
-        const imageElement = document.createElement("img");
-        const captionElement = document.createElement("figcaption");
-        imageElement.src = projetAppart[i].imageUrl;
-        captionElement.innerHTML = projetAppart[i].title;
-        projetsElement.appendChild(imageElement);
-        projetsElement.appendChild(captionElement);
-        gallery.appendChild(projetsElement);
-    }
+    displayProjets(projetAppart);
 }
 
 // afficher les projets Hôtels et Restaurants
-function genererHR(param4) {
+function displayHR(projets) {
     let projetHR = [];
-    for (let i = 0; i < param4.length; i++) {
-        if (param4[i].categoryId == 3) {
-            projetHR.push(param4[i]);
+    for (let i = 0; i < projets.length; i++) {
+        if (projets[i].categoryId == 3) {
+            projetHR.push(projets[i]);
         }
     }
-    for (let i = 0; i < projetHR.length; i++) {
-        const projetsElement = document.createElement("figure");
-        const imageElement = document.createElement("img");
-        const captionElement = document.createElement("figcaption");
-        imageElement.src = projetHR[i].imageUrl;
-        captionElement.innerHTML = projetHR[i].title;
-        projetsElement.appendChild(imageElement);
-        projetsElement.appendChild(captionElement);
-        gallery.appendChild(projetsElement);
-    }
+    displayProjets(projetHR);
 }
 
 // ----------  affichage initial de la page
+const projets = [];
 const projetsAll = fetchProjets();
-projetsAll.then(projets => genererProjets(projets));
-
+projetsAll.then(projets => displayProjets(projets));
+// fetchProjets().then(data => {
+//     data.forEach(projets => projets.push(projets));
+//     displayProjets(projets);
+// })
 
 // ----------  filtrage des projets - (Event listener)
-let filtres = document.getElementsByClassName("filtreOn")
-let filtreOn = filtres[0]
+const filtres = document.getElementsByClassName("filtreOn")
 const filtreTous = document.getElementById("Tous")
 const filtreObjets = document.getElementById("Objets")
 const filtreAppartements = document.getElementById("Appartements")
 const filtreHotelsResto = document.getElementById("HotelsResto")
 
+// changer le bouton actif et vider la gallerie
+function toggleFiltre (boutonActif) {
+    const filtreOn = filtres[0]
+    filtreOn.classList.toggle("filtreOn")
+    boutonActif.classList.toggle("filtreOn")
+    gallery.innerHTML = ("");
+}
+
 // afficher tous les projets
 filtreTous.addEventListener("click", function () {
-    let filtres = document.getElementsByClassName("filtreOn")
-    let filtreOn = filtres[0]
-    filtreOn.classList.remove("filtreOn")
-    filtreTous.classList.add("filtreOn")
-    gallery.innerHTML = ("");
-    projetsAll.then(projets => genererProjets(projets));
+    toggleFiltre (filtreTous);
+    projetsAll.then(projets => displayProjets(projets));
 })
 
 // afficher les projets objets
 filtreObjets.addEventListener("click", function () {
-    let filtres = document.getElementsByClassName("filtreOn")
-    let filtreOn = filtres[0]
-    filtreOn.classList.remove("filtreOn")
-    filtreObjets.classList.add("filtreOn")
-    gallery.innerHTML = ("");
-    projetsAll.then(objets => genererObjets(objets));
+    toggleFiltre (filtreObjets);
+    projetsAll.then(objets => displayObjets(objets));
 })
 
 // afficher les projets Appartements
 filtreAppartements.addEventListener("click", function () {
-    let filtres = document.getElementsByClassName("filtreOn")
-    let filtreOn = filtres[0]
-    filtreOn.classList.remove("filtreOn")
-    filtreAppartements.classList.add("filtreOn")
-    gallery.innerHTML = ("");
-    projetsAll.then(Appart => genererAppart(Appart));
+    toggleFiltre (filtreAppartements);
+    projetsAll.then(Appart => displayAppart(Appart));
 })
 
 // afficher les projets Hôtels et Restaurants
 filtreHotelsResto.addEventListener("click", function () {
-    let filtres = document.getElementsByClassName("filtreOn")
-    let filtreOn = filtres[0]
-    filtreOn.classList.remove("filtreOn")
-    filtreHotelsResto.classList.add("filtreOn")
-    gallery.innerHTML = ("");
-    projetsAll.then(HR => genererHR(HR));
+    toggleFiltre (filtreHotelsResto);
+    projetsAll.then(HR => displayHR(HR));
 })
